e39a26101f7ffb784c61b8589ba6231f512bcc9e
Aymen Ben Hida <aymen.benhmida@gmail.com>
2014-05-09 20:49:38 +0200

    added security checks

--- chap01-mvn/src/main/scala/bootstrap/liftweb/Boot.scala
+++ chap01-mvn/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -25,24 +25,34 @@ class Boot {
     // where to search snippet
     LiftRules.addToPackages("lift.cookbook")
 
+    val canManage_? = If(() => {
+      true
+    }, () => RedirectResponse("/"))
+
+
+    val isAdmin_? = If(() => false, () => {
+      RedirectWithState("/contacts/list",
+        MessageState("Authorized Persons only" -> NoticeType.Warning))
+    })
+
     // Build SiteMap
     def sitemap = SiteMap(
 
       // more complex because this menu allows anything in the
       // /static path to be visible
-      Menu(Loc("Static", Link(List("static"), true, "/static/index"), 
-	       "Static Content")))
+      Menu(Loc("Static", Link(List("static"), true, "/static/index"),
+        "Static Content")))
 
 
     //Init the jQuery module, see http://liftweb.net/jquery for more information.
     LiftRules.jsArtifacts = JQueryArtifacts
-    JQueryModule.InitParam.JQuery=JQueryModule.JQuery172
+    JQueryModule.InitParam.JQuery = JQueryModule.JQuery172
     JQueryModule.init()
 
     //Show the spinny image when an Ajax call starts
     LiftRules.ajaxStart =
       Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
-    
+
     // Make the spinny image go away when it ends
     LiftRules.ajaxEnd =
       Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
@@ -52,7 +62,7 @@ class Boot {
 
     // Use HTML5 for rendering
     LiftRules.htmlProperties.default.set((r: Req) =>
-      new Html5Properties(r.userAgent))    
+      new Html5Properties(r.userAgent))
 
     // Make a transaction span the whole HTTP request
     S.addAround(DB.buildLoanWrapper)
